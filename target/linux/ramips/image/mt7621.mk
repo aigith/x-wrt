jacelift/x-wrt_jacelift

ramips: init support for xwrt_mac500f

target/linux/ramips/image/mt7621.mk
@@ -676,6 +676,15 @@ define Device/xiaomi_mir3p
 endef
 TARGET_DEVICES += xiaomi_mir3p
 
+define Device/xwrt_mac500f
+  MTK_SOC := mt7621
+  IMAGE_SIZE := $(ralink_default_fw_size_16M)
+  DEVICE_VENDOR := XWRT
+  DEVICE_MODEL := MAC500F
+  DEVICE_PACKAGES := uboot-envtools kmod-usb3 kmod-usb-ledtrig-usbport
+endef
+TARGET_DEVICES += xwrt_mac500f
+
 define Device/xwrt_nxc200p
   MTK_SOC := mt7621
   IMAGE_SIZE := $(ralink_default_fw_size_16M)

aigith/x-wrt

target/linux/ramips/image/mt7621.mk
@@ -1296,6 +1296,16 @@ define Device/xiaoyu_xy-c5
 endef
 TARGET_DEVICES += xiaoyu_xy-c5
 
+define Device/xwrt_mac500f
+  $(Device/dsa-migration)
+  SOC := mt7621
+  IMAGE_SIZE := 16064k
+  DEVICE_VENDOR := XWRT
+  DEVICE_MODEL := MAC500F
+  DEVICE_PACKAGES := uboot-envtools kmod-usb3 kmod-usb-ledtrig-usbport
+endef
+TARGET_DEVICES += xwrt_mac500f
+
 define Device/xwrt_nxc200p
   SOC := mt7621
   IMAGE_SIZE := 16064k